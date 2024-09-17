3fe86616f4ee2cd75ea9c71a132bcbfe7e4c0ae3
William Alexander <williamalexander@Williams-MacBook-Air.local>
2024-09-17 21:59:19 +0700

    Tugas 3

--- main/urls.py
+++ main/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path
 from main.views import show_main, create_new_product, show_xml, show_json, show_xml_by_id, show_json_by_id
 
+app_name = "main"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', show_main, name="show_main"),
